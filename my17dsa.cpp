354d386f8d327260c83eea04c14e2d8cd99909ed
Qamar.cpp <hassanabnas876@gmail.com>
2026-09-07 16:57:59 +0500

    my17binarysearch

--- my17dsa.cpp
+++ my17dsa.cpp
@@ -1,31 +1,50 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class myarray
 {
+public:
     int total_size;
     int used_size;
     int *ptr;
 };
-void createarray(myarray * a, int tSize, int uSize){
-    (*a).total_size = tSize;
-    (*a).used_size = uSize;
+void createarray(myarray *a, int tSize, int uSize)
+{
+    a->total_size = tSize;
+    a->used_size = uSize;
+    a->ptr = new int[tSize];
+}
+int binarysearch(int size, int target, int arr[]){
+    int low = 0;
+    int high = size-1;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+    if(arr[mid] == target){
+        return mid;
+    }
+    else if (arr[mid] < target) {
+            low = mid + 1;
+    }
+    else {
+            high = mid - 1; 
+        }
+}
+return -1;
 }
 int main(){
-      myarray();
-      createarray(&marks,100,20);
-    
-    
-}
-arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91] # Must be sorted
-target = 23
+    myarray marks;
+    createarray(&marks, 100, 10);
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 23;
+    int result = binarysearch(size,target ,arr);
+    if (result != -1) {
+        cout << "Found at index: " << result << endl;
+    } else {
+        cout << "Element not found!" << endl;
+    }
+    delete[] marks.ptr;
 
-low = 0
-high = len(arr) - 1while low <= high:
-    mid = (low + high) // 2
-    if arr[mid] == target:
-        print(f"Found at index: {mid}") # Output: Found at index: 5
-        break
-    elif arr[mid] < target:
-        low = mid + 1
-    else:
-        high = mid - 1
+    return 0;
+}
+
+
